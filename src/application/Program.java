c3bf6e1ba56a1992928e59faa7f99379bcd660cf
RamonJacques <ramonjacquesrs@gmail.com>
2023-06-22 16:00:35 -0300

    Program until Contract creation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,32 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Enter contract data:");
+		System.out.print("Contract number: ");
+		int number = sc.nextInt();		
+		System.out.print("Date (dd/MM/yyyy): ");
+		LocalDate date = LocalDate.parse(sc.next(),dtf1);
+		System.out.print("Contract value: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract contract = new Contract(number, date, totalValue);
+		
+		
+		sc.close();
 	}
 }
